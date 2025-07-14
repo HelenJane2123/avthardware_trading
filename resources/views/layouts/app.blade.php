f026d33d8514156afb30df85b81890bd7ae8e2ff
unknown <HelenJane2123>
2025-07-14 16:12:26 +0800

    push update on logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,9 +56,8 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        <i class="fa fa-sign-out fa-lg"></i> Logout
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
